f2edbbc4796dbe8e16c15f59e6705cbf2b174e46
Jake L <jakelovitz@gmail.com>
2019-04-01 14:34:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,4 +21,13 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.shift
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
